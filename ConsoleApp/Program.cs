f1e6d44e700423b5c08875fd591a535df09b6ca5
Carlos Pivotto <carlospivotto@msn.com>
2020-04-17 18:06:29 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Exemplo1();
+    }
+
+    private static void Exemplo1()
+    {
+        //Exemplo do uso de entradas e saídas
+        //Console.Write(): dá a saída SEM pular linha depois.
+        Console.Write("Informe seu nome: ");
+        //Console.ReadLine(): lê toda a entrada (até o usuário dar enter).
+        string nome = Console.ReadLine();
+        //Console.WriteLine: dá a saída e pula uma linha depois.
+        Console.WriteLine("Seja bem-vindo, " + nome + ".");
+    }
+}
