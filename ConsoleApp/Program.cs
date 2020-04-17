f202b1c019af103bbdce5e1ed0ed9d1348dce0ce
Carlos Pivotto <carlospivotto@msn.com>
2020-04-17 19:29:56 -0300

    Alterações

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,7 +4,123 @@ class Program
 {
     static void Main(string[] args)
     {
-        Exemplo1(); 
+        Exemplo8DoWhile();
+
+    }
+
+    private static void Exemplo8DoWhile()
+    {
+        int i = 100;
+        //do-while: SEMPRE realiza a primeira execução.
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i < 5);
+    }
+
+    private static void Exemplo7While()
+    {
+        int n = 0;
+        while (n < 5)
+        {
+            Console.Write("Informe um valor inteiro: ");
+            int idade = Int32.Parse(Console.ReadLine());
+
+            if (idade >= 18 && idade < 65) //ATENÇÃO: A condição SEMPRE entre parênteses.
+            {
+                Console.WriteLine("Adulto.");
+                Console.WriteLine("");
+            }
+            else if (idade < 18)
+            {
+                Console.WriteLine("Menor de idade.");
+            }
+            else
+            {
+                Console.WriteLine("Terceira idade.");
+            }
+
+            n++;
+        }
+    }
+
+    private static void Exemplo6For()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Informe um valor inteiro: ");
+            int idade = Int32.Parse(Console.ReadLine());
+
+            if (idade >= 18 && idade < 65) //ATENÇÃO: A condição SEMPRE entre parênteses.
+            {
+                Console.WriteLine("Adulto.");
+                Console.WriteLine("");
+            }
+            else if (idade < 18)
+            {
+                Console.WriteLine("Menor de idade.");
+            }
+            else
+            {
+                Console.WriteLine("Terceira idade.");
+            }
+        }
+    }
+
+    private static void Exemplo5IfElse()
+    {
+        Console.Write("Informe um valor inteiro: ");
+        int idade = Int32.Parse(Console.ReadLine());
+
+        if (idade >= 18 && idade < 65) //ATENÇÃO: A condição SEMPRE entre parênteses.
+        {
+            Console.WriteLine("Adulto.");
+            Console.WriteLine("");
+        }
+        else if (idade < 18)
+        {
+            Console.WriteLine("Menor de idade.");
+        }
+        else
+        {
+            Console.WriteLine("Terceira idade.");
+        }
+    }
+
+    private static void Exemplo4Matematica()
+    {
+        int x = 5;
+        int y = 3;
+        Console.WriteLine(Math.Pow(x, y));      // potência cúbica (5 elevado a 3) 
+        Console.WriteLine(Math.Pow(x, 1.0 / y));  // raiz cúbica (5 elevado a 1/3) 
+    }
+
+    private static void Exemplo3CalcularSalario()
+    {
+        Console.Write("Informe o valor do salário: R$");
+        double salario = Double.Parse(Console.ReadLine());
+        Console.Write("Informe o reajuste (em %): ");
+        double reajuste = Double.Parse(Console.ReadLine());
+        double novoSalario = Math.Round(salario * (1 + reajuste / 100), 2);
+        string novoSalarioFormat = string.Format("{0:0,0.00}", novoSalario);
+        Console.WriteLine("Com um reajuste de " + reajuste + "%, o novo salário será de R$" + novoSalarioFormat);
+
+        //Outros recursos que os colegas mandaram:
+        // https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings
+        // https://docs.microsoft.com/pt-br/dotnet/api/system.double.tostring?view=netframework-4.7
+    }
+
+    private static void Exemplo2()
+    {
+        //Trabalhando com números
+        // Docs: https://docs.microsoft.com/pt-br/dotnet/csharp/language-reference/builtin-types/integral-numeric-types
+
+        // int ou Int32? Tanto faz:
+        int a = 100;
+        Int32 b = 200;
+
+        Console.WriteLine(a + b);
     }
 
     private static void Exemplo1()
@@ -17,4 +133,6 @@ class Program
         //Console.WriteLine: dá a saída e pula uma linha depois.
         Console.WriteLine("Seja bem-vindo, " + nome + ".");
     }
+
+
 }
